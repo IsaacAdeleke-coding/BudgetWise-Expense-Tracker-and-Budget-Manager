c1b4c320bcd07f1cbdacc15a8843aa8189d336fe
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-11-15 20:11:52 -0800

    Updated header files for BudgetWise: - Expense.hpp: Added tags attribute to Expense struct for better categorization. - Budget.hpp: Introduced categoryBudgets with methods for budget management and over-budget checks. - ExpenseManager.hpp: Defined class methods for expense tracking, budget management, file I/O, and budget alerts. Includes essential functionality for expense tracking and budget management.

--- BudgetWise: Expense Tracker and Budget Manager/main.cpp
+++ BudgetWise: Expense Tracker and Budget Manager/main.cpp
@@ -5,10 +5,3 @@
 //  Created by Isaac Adeleke on 11/15/24.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
