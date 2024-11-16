657523364b1d5bf1a6933ab5ac81efa9abb05f92
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-11-15 20:01:06 -0800

    Initial Commit

---
+++ BudgetWise: Expense Tracker and Budget Manager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BudgetWise: Expense Tracker and Budget Manager
+//
+//  Created by Isaac Adeleke on 11/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
